454c69f2f12bafd6684c6284bfbca0e1e35cb9e1
migue <2023_miguel.vera@iticbcn.cat>
2025-05-22 19:08:23 +0200

    mejora

--- mongoApi/mongoApi/src/main/java/com/iticbcn/mongoApi/Repository/StudentRepository.java
+++ mongoApi/mongoApi/src/main/java/com/iticbcn/mongoApi/Repository/StudentRepository.java
@@ -2,8 +2,10 @@ package com.iticbcn.mongoApi.Repository;
 
 import com.iticbcn.mongoApi.Model.Student;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableReactiveMongoRepositories
 public interface StudentRepository extends ReactiveMongoRepository<Student, String> {
 }
